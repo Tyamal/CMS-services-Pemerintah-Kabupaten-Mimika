80f2982d96e130dfa88403ffa9cf5729e67b44a9
Mal <anityakumala@gmail.com>
2025-02-23 14:19:07 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
-1
+1. install react
 npx create-react-app client
 cd client
 npm install axios react-router-dom
 
 
-2
+2. install back-end
 mkdir server
 cd server
 npm init -y
 npm install express mongoose cors body-parser
 
 
-3
+3. install features dokument upload
+npm install react-dropzone
+
+4.
